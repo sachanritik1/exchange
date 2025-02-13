0d4dfa0900d524bc15448ec10f77d62cc6e383b2
Ritik Sachan <sachanritik1@gmail.com>
2025-02-13 10:42:10 +0530

    add Trades component and integrate with market page; enhance Depth component error handling

---
+++ frontend/src/app/components/depth/Trades.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { getTrades } from "@/app/utils/httpClient";
+import { type Trade } from "@/app/utils/types";
+import React, { useEffect, useState } from "react";
+
+const Trades = ({ market }: { market: string }) => {
+  const [trades, setTrades] = useState<Trade[]>([]);
+
+  useEffect(() => {
+    getTrades(market)
+      .then((_trades) => {
+        setTrades(_trades);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [market]);
+
+  return (
+    <div>
+      <div className="flex justify-between text-xs">
+        <div className="text-white">Price</div>
+        <div className="text-slate-500">Size</div>
+        <div className="text-slate-500">Time</div>
+      </div>
+      {trades.map((trade) => (
+        <div key={trade.id} className="flex justify-between text-xs">
+          <div className="text-slate-500">{trade.price}</div>
+          <div className="text-slate-500">{trade.quantity}</div>
+          <div className="text-slate-500">
+            {new Date(trade.timestamp).toLocaleTimeString()}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Trades;
--- frontend/src/app/trade/[market]/page.tsx
+++ frontend/src/app/trade/[market]/page.tsx
@@ -4,10 +4,25 @@ import { MarketBar } from "@/app/components/MarketBar";
 import { SwapUI } from "@/app/components/SwapUI";
 import { TradeView } from "@/app/components/TradeView";
 import { Depth } from "@/app/components/depth/Depth";
+import Trades from "@/app/components/depth/Trades";
 import { useParams } from "next/navigation";
+import { useState } from "react";
+
+const BookAndTradesTabs = [
+  { label: "Depth", component: Depth },
+  { label: "Trades", component: Trades },
+];
 
 export default function Page() {
   const { market } = useParams();
+
+  const [showDepthORTrades, setShowDepthORTrades] = useState(
+    BookAndTradesTabs[0]?.label,
+  );
+
+  const ActiveComponent = BookAndTradesTabs.find(
+    (item) => item.label === showDepthORTrades,
+  )?.component;
 
   return (
     <div className="flex flex-1 flex-row">
@@ -19,7 +34,18 @@ export default function Page() {
           </div>
           <div className="w-[1px] flex-col border-l border-slate-800"></div>
           <div className="flex w-[250px] flex-col overflow-hidden">
-            <Depth market={market as string} />
+            <div className="grid grid-cols-2 gap-1">
+              {BookAndTradesTabs.map((item) => (
+                <button
+                  key={item.label}
+                  className="flex"
+                  onClick={() => setShowDepthORTrades(item.label)}
+                >
+                  {item.label}
+                </button>
+              ))}
+            </div>
+            {ActiveComponent && <ActiveComponent market={market as string} />}
           </div>
         </div>
       </div>
